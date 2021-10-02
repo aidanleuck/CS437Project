85627b2070516662b6b26f0aaa985190a8da6e74
vmcerda <v_ezlife@yahoo.com>
2021-10-02 16:21:27 -0600

    Added clean csv files

--- Project/constants.py
+++ Project/constants.py
@@ -2,4 +2,4 @@ from os.path import dirname, abspath
 QUERYLOGDIR = "Data/QLog"
 PROJECTDIR = "Project/"
 BASEDIR = dirname(dirname(abspath(__file__)))
-FILEPATH = "BeegFiles\project_1_Wiki_sample_med.csv"
+FILEPATH = 'BeegFiles\project_1_Wiki_sample_med.csv'
