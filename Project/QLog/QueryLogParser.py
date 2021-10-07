dd657974da695b9140b35828507360de66a9c64c
aidanleuck <aidan@leucks.com>
2021-10-06 21:40:51 -0600

    Rough draft of query log parser

--- Project/QLog/QueryLogParser.py
+++ Project/QLog/QueryLogParser.py
@@ -1,10 +1,7 @@
-from genericpath import isfile
-from json import JSONEncoder
 from Models.Query import QueryModel
 import os
 import constants
 import pickle;
-import gzip;
 
 
 # Parses the query logs into a model that can be used for query parsing
@@ -21,59 +18,80 @@ class QueryLogParser:
     def __init__(self):
         self.qLogDir = os.path.join(constants.BASEDIR, constants.PROJECTDIR, constants.QUERYLOGDIR)
         self.frequencyDir = os.path.join(self.qLogDir, constants.FREQUENCYDIR)
+        self.qLogIndex = {}
         if(not(os.path.exists(self.frequencyDir))):
-            os.mkdir(self.frequencyDir)
+            os.makedirs(self.frequencyDir)
 
         
 
     # Merges the query logs together into a dictionary
     def __mergeQueryLogs(self):
-
         # Gets the query log text files
         logFiles = [file for file in os.listdir(self.qLogDir) if(os.path.isfile(os.path.join(self.qLogDir, file)))]
+
+        if(len(logFiles) == 0):
+            raise FileNotFoundError("Please download the Query Log Files and place them in the Data/QLog folder")
 
         # Goes through each query log file and converts into a query model
         for logFile in logFiles:
             logPath = os.path.join(self.qLogDir, logFile)
             fdesc = open(logPath, 'r')
             logContent = fdesc.read()
-            self.__convertFileToModel(logContent)
+            self.__convertFileToIndex(logContent)
+            self.__writeToFrequencyFile()
 
 
     # Converts each line into a query model
     #
     # Params: text, the file text to convert into a query model
-    def __convertFileToModel(self, text):
+    def __convertFileToIndex(self, text):
         lines = text.split("\n")
         lines = lines[1:]
-
         for line in lines:
             logEntry = line.split("\t")
 
             # Makes sure we haven't found an empty line
             if(logEntry[0] != ''):
-        
                 logEntryLength = len(logEntry[1].split(" "))
-                frequencyLogFile = os.path.join(self.frequencyDir, f"frequency-log-{logEntryLength}.pickle")
-                currentJson = {}
-                if(os.path.exists(frequencyLogFile)):
-                    with open(frequencyLogFile, 'rb') as f:
-                        currentJson = pickle.load(f)
-                if logEntry[1] in currentJson.keys():
-                    currentJson[logEntry[1]] +=1
-                else:
-                    currentJson[logEntry[1]] = 1
+                args = [logEntry[0], logEntry[1], logEntry[2]]
 
-                with open(frequencyLogFile, 'w+b') as f:
-                    pickle.dump(currentJson, f, pickle.HIGHEST_PROTOCOL)
+                query = QueryModel(args)
+                
+
+                if logEntryLength in self.qLogIndex.keys():
+                    self.qLogIndex[logEntryLength].append(query)
+                else:
+                    self.qLogIndex[logEntryLength] = [query]
+
+    def __writeToFrequencyFile(self):
+        for key in self.qLogIndex.keys():
+            frequencyFile = os.path.join(self.frequencyDir, f"frequency-file{key}.pkl")
+            frequencyFileDescriptor = open(frequencyFile, "w+b")
+            pickle.dump(self.qLogIndex[key], frequencyFileDescriptor)
+        self.qLogIndex.clear()
+    
+    def __getCandidates(self, query, queryRange):
+        queryLength = len(query.split(" "))
+        startIndex = queryLength
+        endIndex = startIndex + queryRange + 1
+
+        candidateList = []
+        for length in range(startIndex, endIndex):
+            filePath = os.path.join(self.frequencyDir, f"frequency-file{length}.pkl")
+            if(os.path.exists(filePath)):
+                openFileDesc = open(filePath, 'rb')
+                candidateList.append(pickle.load(openFileDesc))
+        return candidateList
         
-    def loadQueryLog(self, query):
-        fileCount = len([file for file in os.listdir(self.frequencyDir) if os.path.isfile(file)])
-        if(os.path.exists(self.frequencyDir) and fileCount > 0):
-            queryLength = len(query.split(" "))
-            self.qLogIndex = pickle.load(os.path.join(self.frequencyDir, f"frequency-log-{queryLength}.pickle"))
+    def loadQueryLog(self, query, queryRange=3):
+        test = os.listdir(self.frequencyDir)
+        fileCount = len([file for file in os.listdir(self.frequencyDir) if not(os.path.isdir(file))])
+        if((os.path.exists(self.frequencyDir) and fileCount > 0)):
+            return self.__getCandidates(query, queryRange)
+            
         else:
             self.__mergeQueryLogs()
+            return self.__getCandidates(query,queryRange)
 
         
         
